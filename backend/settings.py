a3a2170ba060208ceb317ff50749b7eb3264e9a6
raulodev <raulodev@gmail.com>
2023-09-19 10:44:47 -0400

    add jsonrender

--- backend/settings.py
+++ backend/settings.py
@@ -125,9 +125,12 @@ MEDIA_URL = "images/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework_simplejwt.authentication.JWTAuthentication"
-    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
 }
 
 SIMPLE_JWT = {
