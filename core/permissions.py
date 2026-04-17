a7284e79a14b93df48c047b58b94dbedb675f57d
raulodev <raulodev@gmail.com>
2026-04-17 19:35:04 -0400

    refactor(core): simplify permission classes, remove unused `IsOwnerOrReadOnly` and `IsNotAdminOnlyCreate` subclasses

--- core/permissions.py
+++ core/permissions.py
@@ -1,20 +1,9 @@
 from rest_framework import permissions
 
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
+class IAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
 
         return obj.author == request.user
-
-
-class IsNotAdminOnlyCreate(permissions.BasePermission):
-    def has_permission(self, request, view):
-        if request.user.is_authenticated and request.method == "POST":
-            return True
-
-        elif request.method in permissions.SAFE_METHODS and request.user.is_staff:
-            return True
-
-        return False
