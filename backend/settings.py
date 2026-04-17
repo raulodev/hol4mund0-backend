dba630113506e026dbad16ac2e440ddc2a8e3eaa
raulodev <raulodev@gmail.com>
2026-04-17 19:33:34 -0400

    refactor(core): add drf_spectacular and django_filters to backend settings for API documentation and filtering

--- backend/settings.py
+++ backend/settings.py
@@ -33,6 +33,9 @@ INSTALLED_APPS = [
     "corsheaders",
     "autoslug",
     "core",
+    "django_filters",
+    "drf_spectacular",
+    "drf_spectacular_sidecar",
 ]
 
 
@@ -119,7 +122,18 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Hol4 Mund0 API",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+    "SWAGGER_UI_DIST": "SIDECAR",
+    "SWAGGER_UI_FAVICON_HREF": "SIDECAR",
+}
+
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(days=30),
