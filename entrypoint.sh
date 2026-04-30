886b94f1b9375c4d9fc9e3db05f7691f45dcf5d0
raulodev <raulodev@gmail.com>
2026-04-30 11:38:03 -0400

    feat: enhance entrypoint script with additional echo statements for better visibility during initialization

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 set -e
 
+echo "Running migrations..."
 python manage.py migrate
+
+echo "Creating cache table..."
 python manage.py createcachetable
+
+echo "Collecting static..."
 python manage.py collectstatic --noinput
+
+echo "Starting server..."
 
 exec "$@"
